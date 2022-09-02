38054005b27a103f1b68eb6c6ca27e6e85411da1
jun <ggprgrkjh@naver.com>
2022-09-02 13:40:41 +0900

    feat : Loginpage responsive design changed

--- src/components/Mainbackground.tsx
+++ src/components/Mainbackground.tsx
@@ -14,10 +14,10 @@ function Mainbackground(props: MainProps) {
       style={{ backgroundImage: `url(${mainBackground4})` }}
     >
       <div className="text-white absolute flex-col 2xl:mt-mainspacingt5 2xl:ml-mainspacingl5 xl:mt-mainspacingt1 xl:ml-mainspacingl1 md:mt-mainspacingt3 md:ml-mainspacingl3 sm:mt-mainspacingt4 sm:ml-mainspacingl4 ">
-        <div className="md:text-maint3small xl:text-maint1medium sm:text-maint5">
+        <div className="2xl:text-main2xl1 md:text-maint3small xl:text-maint1medium sm:text-maint5">
           인공지능이 찾아주는
         </div>
-        <div className="md:text-maint4small xl:text-maint2medium sm:text-maint6 mt-mainspacingt2 ml-mainspacingl2">
+        <div className="2xl:text-main2xl2 md:text-maint4small xl:text-maint2medium sm:text-maint6 mt-mainspacingt2 ml-mainspacingl2">
           나와 닮은 대한민국의 위인 찾기
         </div>
       </div>
