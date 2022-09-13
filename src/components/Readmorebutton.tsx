1b1e8d9a707f7768ec4ca6e905ca45ca35b7d8d9
jun <ggprgrkjh@naver.com>
2022-09-14 02:57:16 +0900

    feat : Mypage added

---
+++ src/components/Readmorebutton.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Readmorebutton() {
+  return (
+    <button className="absoulte bg-button1 w-[7rem] h-[2rem] text-xs mr-2 rounded-xl text-white	">
+      자세히보기
+    </button>
+  );
+}
+
+export default Readmorebutton;
---
+++ src/pages/Mypage.tsx
@@ -0,0 +1,30 @@
+import Mainbackground2 from "@components/Mainbackground2";
+import React from "react";
+import intro from "@images/intro.png";
+import Slider from "@components/Slider";
+import Slider2 from "@components/Slider2";
+
+function Intropage() {
+  return (
+    <div>
+      <Mainbackground2>
+        <div className="absolute z-40 w-full">
+          <div className="h-header"></div>
+          <div className="h-body w-full ">
+            <div
+              className="absolute w-full h-[28rem] bg-mainorange bg-cover"
+              style={{ background: `url(${intro})`, backgroundSize: "100%" }}
+            ></div>
+            <div className="z-40">
+              <Slider2></Slider2>
+            </div>
+          </div>
+
+          <div className=" h-footer"></div>
+        </div>
+      </Mainbackground2>
+    </div>
+  );
+}
+
+export default Intropage;
