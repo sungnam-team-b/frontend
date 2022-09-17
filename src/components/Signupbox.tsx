7409dedf0b54205e0af8b04840b8db4fb1db8b21
JIYUN LEE <jiton3@naver.com>
2022-09-17 21:17:01 +0900

    api

--- src/components/Signupbox.tsx
+++ src/components/Signupbox.tsx
@@ -11,13 +11,10 @@ function Signupbox() {
   const validationSchema = Yup.object().shape({
     email: Yup.string().email("올바른 이메일 형식이 아닙니다!").required("이메일을 입력하세요."),
     username: Yup.string()
-      .min(2, "닉네임은 2글자 이상입니다.")
-      .max(10, "닉네임은 최대 10글자입니다.")
-      .matches(
-        /^[가-힣a-zA-Z][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
-        "특수문자가 포함되면 안되고 숫자로 시작하면 안됩니다.",
-      )
-      .required("닉네임을 입력하세요."),
+      .min(2, "아이디는 2글자 이상입니다.")
+      .max(10, "아이디는 최대 10글자입니다.")
+      .matches(/^[가-힣a-zA-Z][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/, "다시 입력해주세요.")
+      .required("아이디를 입력하세요."),
     password: Yup.string()
       .min(8, "비밀번호는 최소 8자리 이상입니다.")
       .max(16, "비밀번호는 최대 16자리입니다.")
@@ -26,23 +23,34 @@ function Signupbox() {
         /^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?])[^\s]*$/,
         "대문자, 소문자, 숫자, 특수문자 중 3개 이상 포함해야합니다.",
       ),
-    alias: Yup.string()
+    passwordConfirm: Yup.string()
       .oneOf([Yup.ref("password"), null], "비밀번호가 일치하지 않습니다.")
       .required("필수 입력 값입니다."),
+    alias: Yup.string()
+      .min(2, "닉네임은 2글자 이상입니다.")
+      .max(10, "닉네임은 최대 10글자입니다.")
+      .matches(
+        /^[가-힣a-zA-Z][^!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?\s]*$/,
+        "특수문자가 포함되면 안되고 숫자로 시작하면 안됩니다.",
+      )
+      .required("닉네임을 입력하세요."),
   });
   // const submit = () => {};
   const submit = async (values: any) => {
-    const { email, username, password } = values;
+    const { email, username, password, alias } = values;
     try {
-      await axios.post("/http://localhost:8080/v1/api/users/", {
+      await axios.post("http://localhost:8080/v1/api/users/", {
         email,
         username,
         password,
+        alias,
       });
       setTimeout(() => {
         // 회원가입 후 이동
+        console.log(values);
       }, 2000);
     } catch (e) {
+      console.log(values);
       // 서버에서 받은 에러 메시지 출력
     }
   };
@@ -54,6 +62,7 @@ function Signupbox() {
         username: "",
         password: "",
         alias: "",
+        passwordConfirm: "",
       }}
       validationSchema={validationSchema}
       onSubmit={submit}
@@ -63,8 +72,8 @@ function Signupbox() {
         <div className="box-border rounded-3xl h-72 w-72 p-4">
           <form onSubmit={handleSubmit} autoComplete="off">
             <div className="flex justify-center items-center flex-col">
-              <div className="flex justify-center mb-2">회원가입</div>
-              <div className="grid grid-rows-4">
+              <div className="flex justify-center">회원가입</div>
+              <div className="grid grid-rows-5">
                 <div className="mb-2 mt-2">
                   <div className="grid grid-cols-4">
                     <div className="text-sm font-medium text-black">이메일</div>
@@ -79,6 +88,23 @@ function Signupbox() {
                     id="email"
                     className=" w-52 sm:w-80 p-2.5"
                     placeholder="Email address"
+                    required
+                  />
+                </div>
+                <div className="mb-2 mt-2">
+                  <div className="grid grid-cols-4">
+                    <div className="text-sm font-medium text-black">아이디</div>
+                    <div className="col-span-3 mt-1 text-xxs font-medium text-red-600">
+                      {errors.username}
+                    </div>
+                  </div>
+                  <TextField
+                    value={values.username}
+                    onChange={handleChange}
+                    type="username"
+                    id="username"
+                    className=" w-52 sm:w-80 p-2.5"
+                    placeholder="user name"
                     required
                   />
                 </div>
@@ -103,14 +129,14 @@ function Signupbox() {
                   <div className="grid grid-cols-4">
                     <div className="text-sm font-medium text-black">비밀번호 확인</div>
                     <div className="col-span-3 mt-1 text-xxs font-medium text-red-600">
-                      {errors.alias}
+                      {errors.passwordConfirm}
                     </div>
                   </div>
                   <TextField
-                    value={values.alias}
+                    value={values.passwordConfirm}
                     onChange={handleChange}
                     type="password"
-                    id="alias"
+                    id="passwordConfirm"
                     className=" w-52 sm:w-80 p-2.5"
                     placeholder="•••••••••"
                     required
@@ -120,14 +146,14 @@ function Signupbox() {
                   <div className="grid grid-cols-4">
                     <div className="text-sm font-medium text-black">닉네임</div>
                     <div className="col-span-3 mt-1 text-xxs font-medium text-red-600">
-                      {errors.username}
+                      {errors.alias}
                     </div>
                   </div>
                   <TextField
-                    value={values.username}
+                    value={values.alias}
                     onChange={handleChange}
                     type="string"
-                    id="username"
+                    id="alias"
                     className=" w-52 sm:w-80 p-2.5"
                     placeholder="user name"
                     required
