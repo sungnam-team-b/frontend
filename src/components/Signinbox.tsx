2f5c56a7e5a2f6e61d07d9d15a7677f12a946da2
JIYUN LEE <jiton3@naver.com>
2022-09-17 21:27:29 +0900

    api v2

--- src/components/Signinbox.tsx
+++ src/components/Signinbox.tsx
@@ -18,15 +18,18 @@ function Signinbox() {
   const submit = async (values: any) => {
     const { username, password } = values;
     try {
-      await axios.post("http://localhost:8080/v1/api/users/auth", {
+      const result = await axios.post("http://localhost:8080/v1/api/users/auth", {
         username,
         password,
       });
-
-      console.log("sdss");
+      if (result) {
+        console.log(result.data);
+        alert("로그인 완료");
+      } else {
+        console.log("bbb");
+      }
     } catch (e) {
-      console.log("aaa");
-
+      alert("아이디 혹은 비밀번호를 확인해주세요.");
       // 서버에서 받은 에러 메시지 출력
     }
   };
@@ -50,52 +53,58 @@ function Signinbox() {
     >
       {({ values, handleSubmit, handleChange, errors }) => (
         <div className="flex justify-center items-center flex-col">
-          <div className="relative mb-6">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"></div>
-            <TextField
-              type="username"
-              id="username"
-              className="h-12 w-96 sm:w-96 pl-10 p-2.5 text-white"
-              placeholder="username"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <AccountCircle />
-                  </InputAdornment>
-                ),
-              }}
-              variant="outlined"
-            />
-          </div>
-          <div className="relative mb-6">
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"></div>
-            <TextField
-              type="password"
-              id="password"
-              className="h-12 w-96 sm:w-96 pl-10 p-2.5 "
-              placeholder="password"
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockIcon />
-                  </InputAdornment>
-                ),
-              }}
-              variant="outlined"
-            />
-          </div>
+          <form onSubmit={handleSubmit} autoComplete="off">
+            <div className="relative mb-6">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"></div>
+              <TextField
+                value={values.username}
+                type="username"
+                id="username"
+                onChange={handleChange}
+                className="h-12 w-96 sm:w-96 pl-10 p-2.5 text-white"
+                placeholder="username"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <AccountCircle />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+            </div>
+            <div className="relative mb-6">
+              <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none"></div>
+              <TextField
+                onChange={handleChange}
+                value={values.password}
+                type="password"
+                id="password"
+                className="h-12 w-96 sm:w-96 pl-10 p-2.5 "
+                placeholder="password"
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <LockIcon />
+                    </InputAdornment>
+                  ),
+                }}
+                variant="outlined"
+              />
+            </div>
 
-          <Button
-            type="submit"
-            variant="contained"
-            className="absoulte h-guestloginh1 w-guestloginw1 mt-loginspacing2 bg-blue-400 text-xs md:text-xl z-40"
-          >
-            로그인
-          </Button>
-          <div className="grid grid-cols-2 gap-8 items-center ml-rt mt-loginspacing1 z-50">
-            <div className="text-black">Is this your first visit?</div>
-            <div className="text-blue-300">Sign up</div>
-          </div>
+            <Button
+              type="submit"
+              variant="contained"
+              className="absoulte h-guestloginh1 w-guestloginw1 mt-loginspacing2 bg-blue-400 text-xs md:text-xl z-40"
+            >
+              로그인
+            </Button>
+            <div className="grid grid-cols-2 gap-8 items-center ml-rt mt-loginspacing1 z-50">
+              <div className="text-black">Is this your first visit?</div>
+              <div className="text-blue-300">Sign up</div>
+            </div>
+          </form>
         </div>
       )}
     </Formik>
